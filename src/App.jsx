ef8e65feabf96d8621ded9f3b418cd10ab65ccad
mnkboy <thefouriusmonkeyboy@gmail.com>
2021-01-19 22:51:31 -0600

    Agregamos Iconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Button from '@material-ui/core/Button'
-
+import DeleteIcon from '@material-ui/icons/Delete'
+import { Icon, IconButton } from '@material-ui/core'
 function App() {
   return (
     <Fragment>
@@ -48,6 +49,34 @@ function App() {
       >
         Color
       </Button>
+
+      <DeleteIcon color="primary" />
+
+      <Icon>
+        room
+      </Icon>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<DeleteIcon />}
+      >
+        Delete
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<Icon >room</Icon>}
+      >
+        Delete
+      </Button>
+      <IconButton
+        aria-label="delete"
+      >
+        <DeleteIcon
+          color="primary"
+        />
+
+      </IconButton>
     </Fragment>
 
   );
