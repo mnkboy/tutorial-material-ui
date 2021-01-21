852201acc2759f748ae20944fa107d73ceb25af0
mnkboy <thefouriusmonkeyboy@gmail.com>
2021-01-20 20:49:38 -0600

    Estilo personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,26 @@ import React, { Fragment } from 'react';
 import Button from '@material-ui/core/Button'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { Icon, IconButton, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+//Estilo personalizado
+const useStyle = makeStyles({
+  superstylo: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255,105,135,.3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px'
+  }
+})
 function App() {
+  const clases = useStyle();
   return (
     <Fragment>
+      <Button className={clases.superstylo} fullWidth>
+        Color
+      </Button>
       <Button color="secondary">
         Color
       </Button>
